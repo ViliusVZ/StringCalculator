4f0ccafb14aab311e4f54cecf449cca08b0abd7a
ViliusVZ <46137434+ViliusVZ@users.noreply.github.com>
2020-11-09 15:09:47 +0200

    Added StringCalculator :)

---
+++ Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+	public class Calculator
+	{
+		private List<string> _availableOperators = new List<string>() { "+", "-", "*", "/" };
+
+		public int Calculate(string numbers)
+		{
+			if (string.IsNullOrEmpty(numbers))
+			{
+				throw new FormatException("String null or empty.");
+			}
+
+			var split = numbers.Split(' ');
+
+			if (split.Length < 3)
+			{
+				throw new FormatException("String contains less than 3 members.");
+			}
+
+			var operatorSign = split[0];
+
+			if (!_availableOperators.Contains(operatorSign))
+			{
+				throw new FormatException("Invalid operator.");
+			}
+
+			var allNumbers = ExtractNumbers(split);
+
+			return GetCalculatorOutput(allNumbers, operatorSign);
+		}
+
+		private int GetCalculatorOutput(List<int> numbers, string operatorSign)
+		{
+			var firstNumber = numbers[0];
+
+			for (int i = 1; i < numbers.Count; i++)
+			{
+				switch (operatorSign)
+				{
+					case "+": firstNumber += numbers[i]; break;
+					case "-": firstNumber -= numbers[i]; break;
+					case "*": firstNumber *= numbers[i]; break;
+					case "/": firstNumber /= numbers[i]; break;
+				}
+			}
+
+			return firstNumber;
+		}
+
+		private List<int> ExtractNumbers(string[] numbers)
+		{
+			var allNumbers = new List<int>();
+			int number;
+
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				if (!int.TryParse(numbers[i], out number))
+				{
+					throw new FormatException("Input was not a number.");
+				}
+
+				allNumbers.Add(number);
+			}
+
+			return allNumbers;
+		}
+	}
+}
---
+++ Calculator_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+	[TestFixture]
+	internal class Calculator_Tests
+	{
+		private Calculator calculator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			calculator = new Calculator();
+		}
+
+		private static object[] TestData =
+		{
+			new object[] { "+ 1 1", 2 },
+			new object[] { "- 1 3 4 6", -12 },
+			new object[] { "* 5 8 7", 280 },
+			new object[] { "/ 8 4", 2 }
+		};
+
+		[Test]
+		[TestCaseSource("TestData")]
+		public void Calculator_AllOperands_Test(string input, int result)
+		{
+			var _result = calculator.Calculate(input);
+
+			Assert.AreEqual(result, _result);
+		}
+	}
+}
